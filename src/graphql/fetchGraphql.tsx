c0d076d6b5bc881a0bd6fb542544e8429ee10322
vicwebdev <freedi.lancer@gmail.com>
2021-10-27 13:41:33 +0300

    fix some

--- src/graphql/fetchGraphql.tsx
+++ src/graphql/fetchGraphql.tsx
@@ -1,52 +1,42 @@
-
-export async function fetchProfile() {
-
-    const response = await fetch('mockdata/freelancer.json', {
+async function fetchData(path: string) {
+    const response = await fetch(path, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            'Accept': 'application/json'
+            'Accept': 'application/json',
+            'FLEXHIRE-API-KEY': '2a3ashhf4o6dos4w'
         }
     });
+    return response;
+}
+
+export async function fetchProfile() {
+
+    const response = await fetchData('freelancers/6278');
     // Get the response as JSON
     return await response.json();
 }
 
 export async function fetchJobs() {
 
-    const response = await fetch('mockdata/contracts.json', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
+    const response = await fetchData('contracts');
+
     // Get the response as JSON
     return await response.json();
 }
 
 export async function fetchSuggestQuestions() {
 
-    const response = await fetch('mockdata/suggestedQuestions.json', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
+    const response = await fetchData('questions/suggested');
+
     // Get the response as JSON
     return await response.json();
 }
 
 export async function fetchChatContracts() {
 
-    const response = await fetch('mockdata/chatContracts.json', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    });
+    const response = await fetchData('chat_contacts');
+
     // Get the response as JSON
     return await response.json();
 }
